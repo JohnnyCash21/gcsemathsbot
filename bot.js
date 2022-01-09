ef4c50447dd4da299a14ec39786af8d57c87193a
JohnnyCash21 <59767971+JohnnyCash21@users.noreply.github.com>
2022-01-09 22:01:24 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -88,7 +88,7 @@ Client.on('message', async (message)=>{
         .addField("`" + prefix + 'ytstats`', "YouTube statistics for The GCSE Maths Tutor", true)
         .addField("`" + prefix + 'hello`', "oh my god oh my god its reeeall, gcse maths tutor has hi to me ohhhh", true)
         .addField("`" + prefix + 'poll`', "Voting poll for any decision makings", true)
-        .setFooter('Bot Made By: RabilA#2691')
+        .setFooter('Bot Made By: RabilA#4257')
         .setThumbnail(image2)
         .setColor(0xF1C40F)
         message.channel.send(commandsEmbed)
